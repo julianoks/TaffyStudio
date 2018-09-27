9c3067a0a9d82d182565dd440d8d18f6892c79f1
julianoks <juao15@hampshire.edu>
2018-09-27 19:40:53 -0400

    link inputDescription sidebar to data

--- js/sidebar/inputDescriptions.js
+++ js/sidebar/inputDescriptions.js
@@ -8,7 +8,7 @@ function parseToIntArray(str){
 	return ints.includes(NaN)? false : ints
 }
 
-function makeInputDescRow(onchange, shape=[], dtype='float32'){
+function makeInputDescRow(oninput, shape=[], dtype='float32'){
 	let li = document.createElement('li')
 	li.className = 'list-group-item'
 	// dtypeSelector
@@ -21,12 +21,12 @@ function makeInputDescRow(onchange, shape=[], dtype='float32'){
         if(dtype===name){ option.selected = true }
 		dtypeSelector.appendChild(option)
 	})
-	dtypeSelector.onchange = onchange
+	dtypeSelector.oninput = oninput
 	// shapeTextBox
 	let shapeTextBox = document.createElement('input')
     shapeTextBox.setAttribute('placeholder', 'e.g. "1, 2, 3"')
     shapeTextBox.value = shape.join(', ')
-	shapeTextBox.onchange = onchange
+	shapeTextBox.oninput = oninput
 	// table
 	let table = document.createElement('table')
 	table.style.border = 'none'
@@ -48,7 +48,13 @@ function makeInputDescRow(onchange, shape=[], dtype='float32'){
 	return li
 }
 
-const makeOnChange = (ownerSVG) => () => {
+const makeOnInput = (descList, ownerSVG) => () => {
+    Array.from(descList.children).forEach((ele, i) => {
+        const name = `INPUT_${i}`
+        const shape = parseToIntArray(descList.querySelector('input').value)
+        const dtype = descList.querySelector('select').value
+        ownerSVG.__data__.moduleMetaData.inputDescriptions[name] = {shape, dtype}
+    })
 }
 
 export function makeInputDescCard(ownerSVG, vertexName){
@@ -56,13 +62,13 @@ export function makeInputDescCard(ownerSVG, vertexName){
     list.className = 'list-group'
     const {inputDescriptions} = ownerSVG.__data__.moduleMetaData
     const nInputs = getNumInputsOutputs(ownerSVG, vertexName).nOut
-    const onchange = makeOnChange(ownerSVG)
+    const oninput = makeOnInput(list, ownerSVG)
     for(let i=0; i<nInputs; i++){
         if(inputDescriptions.hasOwnProperty(`INPUT_${i}`)){
             const {shape, dtype} = inputDescriptions[`INPUT_${i}`]
-            list.appendChild(makeInputDescRow(onchange, shape, dtype))
+            list.appendChild(makeInputDescRow(oninput, shape, dtype))
         } else {
-            list.appendChild(makeInputDescRow(onchange, [], 'float32'))
+            list.appendChild(makeInputDescRow(oninput, [], 'float32'))
         }
     }
     const panelHTML = '<div class="panel panel-default"> <div class="panel-heading"> <h3 class="panel-title">Input Descriptions</h3></div> <div class="panel-body"></div> </div>'
