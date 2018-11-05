976190edd0ec00e046231b63db6e88b48d833c90
julianoks <juao15@hampshire.edu>
2018-11-05 18:12:29 -0500

    get rid of module_1

--- tutorial/utils.js
+++ tutorial/utils.js
@@ -10,8 +10,10 @@ export function displayInteractive(holder, focusModule, showOnly, fullBase, inpD
     let state = {base, imports, invisible}
 
     let studio = newStudio(holder, [window.innerWidth*0.8,window.innerHeight*0.8,], state)
-    d3.select(`${holder} #${focusModule}`).node().click()
     d3.select(`${holder} #module_1`).node().parentElement.remove()
+    studio.__data__.moduleHolders.module_1.remove()
+    delete studio.__data__.moduleHolders.module_1
+    d3.select(`${holder} #${focusModule}`).node().click()
     const svgData = studio.__data__.moduleHolders[focusModule].querySelector('svg').__data__
     svgData.moduleMetaData.inputDescriptions = inpDesc
     svgData.givePorts('Inputs', 0, Object.keys(inpDesc).length)
